62a2b86437b8ea11323f74130a8aa24cfc02d344
WithAPorpoise <anathama.ware@gmail.com>
2022-11-09 16:53:08 -0800

    Removed implementation for workorder interface, added virtual functions to interface

--- workorder.cpp
+++
@@ -1,6 +0,0 @@
-#include "workorder.h"
-
-WorkOrder::WorkOrder()
-{
-
-}
--- workorder.h
+++ workorder.h
@@ -1,11 +1,14 @@
 #ifndef WORKORDER_H
 #define WORKORDER_H
-
+#include message.h
 
 class WorkOrder
 {
 public:
-    WorkOrder();
+	vitual	Message getCommand();
+	virtual Message getData();
+	virtual Message getSecurity();
+
 };
 
 #endif // WORKORDER_H
