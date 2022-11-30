926944655d4142826f00a7a60032abe8b1e3ed6e
WithAPorpoise <anathama.ware@gmail.com>
2022-11-29 21:16:04 -0800

    Added functions to module for receiving workorders and letting the backbone know when the module is waiting.

--- module.h
+++ module.h
@@ -1,14 +1,20 @@
 #ifndef MODULE_H
 #define MODULE_H
 #include <string>
+#include <workorder.h>
 class Module {
 protected:
     int _ID;
     std::string name;
 public :
+    bool waiting=false;
     virtual int getID()=0;
     virtual void setID(int id)=0;
     virtual std::string getName()=0;
+    //Use this to update waiting flag.
+    virtual bool setWaiting()=0;
+    //Backbone will use this to deliver jobs
+    virtual bool receive(WorkOrder& wo);
 };
 
 #endif //MODULE_H
