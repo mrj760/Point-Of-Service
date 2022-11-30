7ba2c8b7db65a11814920d0115a1d8b0139eff87
WithAPorpoise <anathama.ware@gmail.com>
2022-11-30 11:32:46 -0800

    Removed virtual functions from workorder, made comparison operator directly access passed workorder's messages, rather than use getter methods

--- workorder.h
+++ workorder.h
@@ -6,16 +6,12 @@ class WorkOrder
 {
     virtual ~WorkOrder() = 0;
 public:
-	virtual	Message* getCommand();
-	virtual Message* getData();
-	virtual Message* getSecurity();
     int ID;
     Message command;
     Message data;
     Message security;
     bool operator ==(WorkOrder* wo){
-//        return (getCommand() == wo.getCommand() && getData() == wo.getData() && ID == wo.ID);
-        return (getCommand() == wo->getCommand() && getData() == wo->getData() && ID == wo->ID);
+        return (command == &(wo->command) && data == &(wo->data) && ID == wo->ID);
     }
 };
 
